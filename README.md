1b531ea6d6ac4f64106192236a43ed3dc81e9704
Christian González Di Antonio <christiangda@gmail.com>
2024-06-24 12:37:41 +0200

    feat: wip with internal dependencies

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # spectre-dependencies
+
 This is the project used to install the spectre project dependencies in the OS
+
+## Installation
+
+```bash
+git clone
+cd spectre-dependencies
+
+# default SPECTRE_INSTALL_DIR=$HOME/spectre-dependencies
+cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release -DSPECTRE_INSTALL_DIR=<path to install spectre dependencies>
+cmake --build build --parallel <number of cores you want to use>
+```
--- cmake/ExternalProject-DepthAI.cmake
+++ cmake/ExternalProject-DepthAI.cmake
@@ -34,6 +34,38 @@ ExternalProject_Add( ${DEPENDENCY_NAME}
     -D DEPTHAI_BUILD_EXAMPLES=OFF
     -D OpenCV_DIR=${OpenCV_DIR}
 )
+# Set the include and library paths
+# Directories must be created before adding dependencies
+# this is because in the configure step, these directories are used
+file(MAKE_DIRECTORY ${DEPTHAI_PREFIX_DIR}/lib)
+file(MAKE_DIRECTORY ${DEPTHAI_PREFIX_DIR}/include)
+
+#  depthai::core
+add_library(depthai::core STATIC IMPORTED GLOBAL)
+set_target_properties(depthai::core PROPERTIES
+      IMPORTED_LOCATION ${DEPTHAI_PREFIX_DIR}/lib/libdepthai-core.${DEPTHAI_LIBRARY_SUFFIX}
+      INTERFACE_INCLUDE_DIRECTORIES ${DEPTHAI_PREFIX_DIR}/include
+)
+
+add_dependencies(depthai::core ${DEPENDENCY_NAME})
+
+#  depthai::opencv
+add_library(depthai::opencv STATIC IMPORTED GLOBAL)
+set_target_properties(depthai::opencv PROPERTIES
+      IMPORTED_LOCATION ${DEPTHAI_PREFIX_DIR}/lib/libdepthai-opencv.${DEPTHAI_LIBRARY_SUFFIX}
+      INTERFACE_INCLUDE_DIRECTORIES ${DEPTHAI_PREFIX_DIR}/include
+)
+
+add_dependencies(depthai::opencv ${DEPENDENCY_NAME})
+
+# depthai::resources
+add_library(depthai::resources STATIC IMPORTED GLOBAL)
+set_target_properties(depthai::resources PROPERTIES
+      IMPORTED_LOCATION ${DEPTHAI_PREFIX_DIR}/lib/libdepthai-resources.${DEPTHAI_LIBRARY_SUFFIX}
+      INTERFACE_INCLUDE_DIRECTORIES ${DEPTHAI_PREFIX_DIR}/include
+)
+
+add_dependencies(depthai::resources ${DEPENDENCY_NAME})
 
 set(DEPTHAI_INCLUDE_DIR ${DEPTHAI_PREFIX_DIR}/include)
 set(DEPTHAI_LIB_DIR ${DEPTHAI_PREFIX_DIR}/lib)
